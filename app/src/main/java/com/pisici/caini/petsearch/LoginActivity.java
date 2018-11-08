c32641efb444a605dc2a0302407281cf840c9700
Alex Gherghe <alexluciangherghe@gmail.com>
2018-11-08 16:09:09 +0200

    Login progress

--- app/src/main/java/com/pisici/caini/petsearch/LoginActivity.java
+++ app/src/main/java/com/pisici/caini/petsearch/LoginActivity.java
@@ -1,10 +1,19 @@
 package com.pisici.caini.petsearch;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,8 +21,7 @@ public class LoginActivity extends AppCompatActivity {
     EditText mPasswordEt;
     Button mLoginBtn;
     Button mSignupBtn;
-    /*private FirebaseAuth mAuth;
-    User user;*/
+    private FirebaseAuth mAuth;
     ImageView gif;
     ImageView logo;
     Button mresend_verificationBtn;
@@ -23,5 +31,96 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //mAuth = FirebaseAuth.getInstance();
+        mEmailEt = (EditText) findViewById(R.id.login_usernameEt);
+        mPasswordEt = (EditText) findViewById(R.id.login_passwordEt);
+        mLoginBtn = (Button) findViewById(R.id.loginBtn);
+        mSignupBtn = (Button) findViewById(R.id.login_signupBtn);
+        mresend_verificationBtn = (Button) findViewById(R.id.resend_verificationBtn);
+        mreset_passwordBtn = (Button) findViewById(R.id.reset_passwordBtn);
+        mAuth = FirebaseAuth.getInstance();
+        mresend_verificationBtn.setEnabled(false);
+
+        //Verifica daca esti conectat la internet
+        if (!verifyInternetConnectivty())
+            makeToast("Please connect to the internet");
+        //daca esti deja logat te duce direct in menu activity
+        if (mAuth.getCurrentUser() != null) {
+            retrieve_user();
+        }
+        //daca nu esti logat
+        else {
+
+
+            //OnClick care duce la activitatea de signup
+           /* mSignupBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(LoginActivity.this, SignupActivity.class);
+                    startActivityForResult(i, 1);
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
+                }
+            });
+
+            //OnClick pentru logare
+            mLoginBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    login();
+                }
+            });
+
+        }
+        mreset_passwordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetpassword();
+            }
+        });*/
+
+    }
+    }
+    //functie care verifica daca esti conectat la net
+    protected boolean verifyInternetConnectivty() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+            return true;
+        else
+            return false;
+    }
+
+    void makeToast(String string){
+        Toast.makeText(LoginActivity.this, string,Toast.LENGTH_SHORT).show();
+    }
+    //pune datele in obiectul
+    public void retrieve_user() {
+        /*final DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        final String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference myRef = database;
+        user = new User();
+
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.v("log", "onDataChange din retrieve_user din loginActivity");
+                user = dataSnapshot.child("id").child(uid).getValue(User.class);
+                String LobbyID = dataSnapshot.child("id").child(uid).child("Lobby").getValue(String.class);
+                //daca nu, te duce in meniu
+                Log.v("log", "trimis in meniu din onDataChange");
+                Intent i = new Intent(LoginActivity.this, MenuActivity.class);
+                i.putExtra("User", user);
+                startActivity(i);
+                finish();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(LoginActivity.this, databaseError.toException().toString(), Toast.LENGTH_LONG).show();
+            }
+
+        });*/
+
     }
 }
---
+++ app/src/main/java/com/pisici/caini/petsearch/User.java
@@ -0,0 +1,9 @@
+package com.pisici.caini.petsearch;
+
+public class User {
+    private String First_name = "nu a mers:(";
+    private String Last_name = "null";
+    private String Email= "null";
+    private String Phone= "07noidoi";
+
+}
